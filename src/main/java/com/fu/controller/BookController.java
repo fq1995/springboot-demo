0aa9661f45a8b248eccace76959a2df368266715
付强 <1152237929@qq.com>
2017-12-10 10:56:36 +0800

    use thymeleaf3

--- src/main/java/com/fu/controller/BookController.java
+++ src/main/java/com/fu/controller/BookController.java
@@ -1,7 +1,12 @@
 package com.fu.controller;
 
+import com.fu.pojo.Book;
+import com.fu.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by fu on 2017/12/9.
@@ -9,8 +14,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("/books")
-    public String list(){
+    public String list() {
         return "books";
     }
+
+    @GetMapping("/books/{id}")
+    public String detail(@PathVariable long id, Model model){
+        Book book = bookService.findOne(id);
+        model.addAttribute("book", book);
+        return "book";
+    }
+
 }
