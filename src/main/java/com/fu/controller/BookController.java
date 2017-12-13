9aaf68464009bdca9d4b50a9d2c0377de2e05a0a
付强 <1152237929@qq.com>
2017-12-13 23:02:10 +0800

    input/addBook

--- src/main/java/com/fu/controller/BookController.java
+++ src/main/java/com/fu/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +20,11 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    /**
+     * 书单列表
+     * @param model
+     * @return
+     */
     @GetMapping("/books")
     public String list(Model model) {
         List<Book> bookList = bookService.findAll();
@@ -26,6 +32,12 @@ public class BookController {
         return "books";
     }
 
+    /**
+     * 书单详情
+     * @param id
+     * @param model
+     * @return
+     */
     @GetMapping("/books/{id}")
     public String detail(@PathVariable long id, Model model){
         Book book = bookService.findOne(id);
@@ -36,4 +48,21 @@ public class BookController {
         return "book";
     }
 
+    /**
+     * 跳转input提交页面
+     * @return
+     */
+    @GetMapping("/books/input")
+    public String inputPage(){
+        return "input";
+    }
+
+    /**
+     *提交一个书单信息
+     */
+    @PostMapping("/books")
+    public String post(Book book){
+        bookService.save(book);
+        return "redirect:/books";
+    }
 }
