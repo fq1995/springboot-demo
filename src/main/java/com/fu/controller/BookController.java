99c22d2a45d751b45ee8624efae8f5afca7da9a1
付强 <1152237929@qq.com>
2017-12-14 23:22:09 +0800

    update book

--- src/main/java/com/fu/controller/BookController.java
+++ src/main/java/com/fu/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class BookController {
 
     /**
      * 书单列表
+     *
      * @param model
      * @return
      */
@@ -34,14 +36,15 @@ public class BookController {
 
     /**
      * 书单详情
+     *
      * @param id
      * @param model
      * @return
      */
     @GetMapping("/books/{id}")
-    public String detail(@PathVariable long id, Model model){
+    public String detail(@PathVariable long id, Model model) {
         Book book = bookService.findOne(id);
-        if(null == book){
+        if (null == book) {
             book = new Book();
         }
         model.addAttribute("book", book);
@@ -50,19 +53,38 @@ public class BookController {
 
     /**
      * 跳转input提交页面
+     *
      * @return
      */
     @GetMapping("/books/input")
-    public String inputPage(){
+    public String inputPage(Model model) {
+        Book book = new Book();
+        model.addAttribute("book", book);
         return "input";
     }
 
     /**
-     *提交一个书单信息
+     * 跳转到更新页面input
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("books/input/{id}")
+    public String inputEditPage(@PathVariable long id, Model model) {
+        Book book = bookService.findOne(id);
+        model.addAttribute("book", book);
+        return "input";
+    }
+
+    /**
+     * 提交一个书单信息
      */
     @PostMapping("/books")
-    public String post(Book book){
-        bookService.save(book);
+    public String post(Book book, final RedirectAttributes attributes) {
+        Book book1 = bookService.save(book);
+        if(book1 != null){
+            attributes.addFlashAttribute("message","《"+book1.getName()+"》 信息提交成功");
+        }
         return "redirect:/books";
     }
 }
